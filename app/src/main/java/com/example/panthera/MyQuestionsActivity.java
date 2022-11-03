27552e55511777ff8baea39bb084b1c54c43ca31
madda01 <it20232368@my.sliit.lk>
2022-11-03 20:00:27 +0530

    Revert "folder structure"
    This reverts commit 46b223ac0ea0c8b63b250b4e5fef1726f5600792.

---
+++ app/src/main/java/com/example/panthera/MyQuestionsActivity.java
@@ -0,0 +1,14 @@
+package com.example.panthera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyQuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_questions);
+    }
+}
