3a0e843ae2b522d85ce827daf24bcc733b8358e8
madda01 <it20232368@my.sliit.lk>
2022-11-03 20:08:18 +0530

    folder structure

--- app/src/main/java/com/example/panthera/MainActivity.java
+++ app/src/main/java/com/example/panthera/MainActivity.java
@@ -1,16 +1,14 @@
 package com.example.panthera;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class PostQuestionActivity<adapter> extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_post_question);
+        setContentView(R.layout.activity_main);
     }
-
-
 }
-
