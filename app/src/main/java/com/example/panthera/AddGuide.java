ea1df7428823508533cf65165e216b03c59dcb13
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2022-11-05 00:24:41 +0530

    Create add a safari guide

--- app/src/main/java/com/example/panthera/AddGuide.java
+++ app/src/main/java/com/example/panthera/AddGuide.java
@@ -1,10 +1,12 @@
 package com.example.panthera;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,9 +14,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -30,10 +39,23 @@ public class AddGuide extends AppCompatActivity {
     LinearLayout linearLayout;
     Uri ImageUri;
 
+    private FirebaseDatabase database;
+    private FirebaseStorage firebaseStorage;
+    ProgressDialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_guide);
+
+        database = FirebaseDatabase.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+        dialog = new ProgressDialog(this);
+        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        dialog.setMessage("Please Wait...");
+        dialog.setCancelable(false);
+        dialog.setTitle("Guide Adding");
+        dialog.setCanceledOnTouchOutside(false);
 
         guide_name = findViewById(R.id.guide_name);
         guide_description = findViewById(R.id.guide_description);
@@ -65,6 +87,48 @@ public class AddGuide extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                dialog.show();
+
+                final StorageReference reference = firebaseStorage.getReference().child("guide")
+                        .child(System.currentTimeMillis() + "");
+
+                reference.putFile(ImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                    GuideModel model = new GuideModel();
+                                    model.setImage1(uri.toString());
+
+                                    model.setGuide_name(guide_name.getText().toString());
+                                    model.setGuide_address(guide_address.getText().toString());
+                                    model.setGuide_contact(guide_contact.getText().toString());
+                                    model.setGuide_description(guide_description.getText().toString());
+                                    model.setGuide_languages(guide_languages.getText().toString());
+                                    
+                                    database.getReference().child("guide").push().setValue(model)
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void unused) {
+
+                                                    Toast.makeText(AddGuide.this, "Guide Added Successfully!", Toast.LENGTH_SHORT).show();
+                                                    dialog.dismiss();
+                                                }
+                                            }).addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+
+                                                    dialog.dismiss();
+                                                    Toast.makeText(AddGuide.this, "Error Occurred", Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+                            }
+                            
+                        });
+
+                    }
+                });
             }
         });
 
