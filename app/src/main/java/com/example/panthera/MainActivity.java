793e7bf31e0324ac5b8417dbe523ee8eac4c066a
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2022-11-04 19:42:53 +0530

    create UPLOAD IMAGE button

--- app/src/main/java/com/example/panthera/MainActivity.java
+++ app/src/main/java/com/example/panthera/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.panthera;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button add;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        add = findViewById(R.id.add);
+        recyclerView = findViewById(R.id.recycleView);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddGuide.class);
+                startActivity(intent);
+            }
+        });
     }
 }
