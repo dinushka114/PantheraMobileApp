b981c4cec3c0e8a512d082d5b4f905f88517745c
Dinesh_Saman <it20653286@my.sliit.lk>
2022-11-06 20:25:41 +0530

    Add question and question forum .

--- app/src/main/java/com/example/panthera/MainActivity.java
+++ app/src/main/java/com/example/panthera/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.panthera;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
